3a7b4a3a709fef842d1ad0d7052f776b1542b869
AbdullahMufti <abdullahmufty@gmail.com>
2023-03-15 18:16:16 +0500

    all-serverside

--- pages/BrowseEnglishQuestions/QuestionCard.js
+++ pages/BrowseEnglishQuestions/QuestionCard.js
@@ -1,5 +1,8 @@
-"use client";
-import { GetDateHexa, GetDate, StyleRemover } from "../../lib/univ-functions";
+import {
+  GetDateHexa,
+  GetDate,
+  StyleRemover,
+} from "../../lib/univ-functions-server";
 import { Link } from "next/link";
 import HTMLReactParser from "html-react-parser";
 import { useRouter } from "next/router";
@@ -79,7 +82,7 @@ export default function Questioncard({ EnQuestions, SKIP }) {
               <path
                 fillRule="evenodd"
                 d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
             Prev
@@ -99,7 +102,7 @@ export default function Questioncard({ EnQuestions, SKIP }) {
               <path
                 fillRule="evenodd"
                 d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
